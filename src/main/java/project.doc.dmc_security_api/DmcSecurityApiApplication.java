107bc4a3dde666cc61354009e779679fc33f9316
deepak <deepakg4283@gmail.com>
2024-01-06 00:38:58 +0530

    Project setup

---
+++ src/main/java/project.doc.dmc_security_api/DmcSecurityApiApplication.java
@@ -0,0 +1,13 @@
+package dmc_security_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmcSecurityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmcSecurityApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dmc_security_api/DmcSecurityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dmc_security_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmcSecurityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
